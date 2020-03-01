5da16f215dfe53a17dfd3dc81bb4d07026a5d99e
librarylai <library@intella.co>
2020-03-01 11:36:50 +0800

    修改public 路徑

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './src/public')));
 
 app.get('/', function (req, res) {
   res.send('index');
