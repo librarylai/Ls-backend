b52e30bfb2a3770515f2da6223bf08d0d530097f
librarylai <library@intella.co>
2020-03-02 01:01:41 +0800

    test

--- src/routes/member.js
+++ src/routes/member.js
@@ -1,6 +1,7 @@
 let express = require('express')
 let router = express.Router()
 import MemberModifyController from '../controllers/modify_controller.js'
+console.log('MemberModifyController', new MemberModifyController().postRegister)
 let memberModifyController = new MemberModifyController()
 router.post('/register', memberModifyController.postRegister)
 router.post('/login', memberModifyController.postLogin)
