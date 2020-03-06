d338817d193dfa53a2c0d50ce4148f2dd2891a22
librarylai <library@intella.co>
2020-03-06 16:36:41 +0800

    0306

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
-// const nodeExternals = require('webpack-node-externals');
+const nodeExternals = require('webpack-node-externals');
 const path = require('path');
 
 module.exports = {
     target: 'node',
-    // externals: [nodeExternals()],
+    externals: [nodeExternals()],
     entry: ['./app.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
-        libraryTarget: 'commonjs2'
     },
     module: { //設定你的檔案選項
         rules: [{
