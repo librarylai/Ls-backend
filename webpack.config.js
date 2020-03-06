28361b5eb0d31706117273c177a1efc8bc697f3b
librarylai <library@intella.co>
2020-03-06 16:35:02 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const nodeExternals = require('webpack-node-externals');
+// const nodeExternals = require('webpack-node-externals');
 const path = require('path');
 
 module.exports = {
     target: 'node',
-    externals: [nodeExternals()],
+    // externals: [nodeExternals()],
     entry: ['./app.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
